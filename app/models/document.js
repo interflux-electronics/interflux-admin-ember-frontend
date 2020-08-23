9b49ece6bef9834e8541bffbbfb2cbdb14f1b63f
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-08-23 20:20:58 +1000

    Load document categories from the API

--- app/models/document.js
+++ app/models/document.js
@@ -1,8 +1,11 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
+import { alias } from '@ember/object/computed';
 
 export default class DocumentModel extends Model {
   @attr('string') path;
   @attr('string') name;
 
   @belongsTo('language') language;
+  @belongsTo('document-category') documentCategory;
+  @alias('documentCategory') category;
 }
