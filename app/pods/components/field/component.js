a1a4646dbd844330307afb76d0d9c53d77ae5600
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-06 17:18:41 +1000

    Split concerns into <Input::String> and <Field> to seperate concerns

--- app/pods/components/field/component.js
+++ app/pods/components/field/component.js
@@ -1,3 +1,23 @@
 import Component from '@glimmer/component';
 
-export default class FieldComponent extends Component {}
+// locked          show lock icon
+// no-changes      show nothing
+// dirty-invalid   warning
+// dirty-valid     save button
+// saving          disabled save button "Saving...""
+// save-success    Done! --> timeout to idle
+// save-fail       Error message --> hide on dismiss or when user starts correcting
+
+export default class FieldComponent extends Component {
+  get classes() {
+    return [this.size, this.theme].join(' ');
+  }
+
+  get size() {
+    return this.args.size || 'medium';
+  }
+
+  get theme() {
+    return this.args.theme || 'primary';
+  }
+}
