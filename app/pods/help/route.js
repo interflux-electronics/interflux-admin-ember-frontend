a54f63d54af4df421072ad64dcd0bca482c2e62e
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-06 17:19:11 +1000

    Remove unused help route

--- app/pods/help/route.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class HelpRoute extends Route {
-  model(params, transition) {
-    let backRoute = transition.from ? transition.from.name : 'index';
-    return { backRoute };
-  }
-}
