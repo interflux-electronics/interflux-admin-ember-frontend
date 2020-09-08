669d90742cc94d806d7965ea6030ce40ba6ec870
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-08 20:22:51 +1000

    Split the remote setup scripts into 2

--- remote/setup.sh
+++ remote/setup.sh
@@ -3,9 +3,11 @@
 set -e
 set -o pipefail
 
-user=bot
+user1=bot
+user2=jw
 server=server.interflux.com
 domain=admin.interflux.com
+redirect_www=false
 repo=git@github.com:interflux-electronics/admin.interflux.com.git
 
 echo "----------"
@@ -14,6 +16,10 @@ echo "----------"
 
 (
   set -x
-  scp -i ~/.ssh/$user@$server remote/setup-remote.sh $user@$server:~/
-  ssh -i ~/.ssh/$user@$server -t $user@$server "~/setup-remote.sh $domain $repo; rm -f ~/setup-remote.sh"
+
+  scp -i ~/.ssh/$user1@$server remote/setup-remote-1.sh $user1@$server:~/
+  ssh -i ~/.ssh/$user1@$server -t $user1@$server "~/setup-remote-1.sh $domain $repo; rm -f ~/setup-remote-1.sh"
+
+  scp remote/setup-remote-2.sh $user2@$server:~/
+  ssh -t $user2@$server "~/setup-remote-2.sh $domain $redirect_www; rm -f ~/setup-remote-2.sh"
 )
