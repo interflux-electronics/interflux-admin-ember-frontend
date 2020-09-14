4b2853b7d614c725f5db2b7db508f446c1cf3516
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-14 22:01:34 +1000

    Remove comments

--- app/initializers/log-git.js
+++ app/initializers/log-git.js
@@ -1,9 +1,8 @@
 import ENV from 'interflux/config/environment';
 
-export function initialize(/* application */) {
+export function initialize() {
   console.debug('DEBUG: Git revision :', ENV.gitRevision);
   console.debug('DEBUG: Git branch   :', ENV.gitBranch);
-  console.debug('DEBUG: -------------------------------');
 }
 
 export default {
