d65e16358a76f8da81030c9a9ad291d16c6e1fa5
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-11-07 17:46:38 +1100

    Improve git logging

--- app/initializers/log-git.js
+++ app/initializers/log-git.js
@@ -3,6 +3,7 @@ import ENV from 'interflux/config/environment';
 export function initialize() {
   console.debug('DEBUG: Git revision :', ENV.gitRevision);
   console.debug('DEBUG: Git branch   :', ENV.gitBranch);
+  console.debug('DEBUG: ------------------------------');
 }
 
 export default {
