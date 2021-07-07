86a39ef26c2870d079a5c50d1d31a5840403d721
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-07-07 15:53:50 +1000

    Align build config with Public

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,13 +6,13 @@ const ENV = require('./config/environment')(EmberApp.env());
 // To make CSS environment aware we assign a new src to output map.
 // The src changes with the environment.
 const cssMap = {};
-
-cssMap[ENV.environment] = `/assets/app.css`;
+cssMap[ENV.environment] = '/assets/app.css';
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     outputPaths: {
       app: {
+        js: '/assets/app.js',
         css: cssMap
       }
     },
@@ -30,44 +30,13 @@ module.exports = function (defaults) {
       remove: false
     },
 
-    // Prevent CSS minification in development and tests
-    minifyCSS: {
-      enabled: ENV.isProduction
-    },
-
-    // Prevent JS minification in development and tests
-    minifyJS: {
-      enabled: ENV.isProduction
-    },
-
-    // Enable source maps in all environments because it helps debugging.
-    sourcemaps: {
-      enabled: true,
-      extensions: ['js']
-    },
-
+    // Because the 2 JS and 2 CSS bundles are being HTTP2 pushed alongside the
+    // index.html by Nginx, we need the name to be predictable and thus have
+    // no fingerprints. Cache busting is not needed because the JS and CSS
+    // bundles we'll never cache (they update too often). Images, videos and
+    // fonts we'll highly cache though.
     fingerprint: {
-      enabled: ENV.isProduction,
-      extensions: [
-        'js',
-        'css',
-        'png',
-        'jpg',
-        'svg',
-        'map',
-        'mp4',
-        'ogg',
-        'webp',
-        'webm',
-        'woff',
-        'woff2'
-      ],
-      replaceExtensions: ['html', 'css', 'scss', 'js'],
-      customHash: ENV.gitRevision
-    },
-
-    'ember-fetch': {
-      preferNative: true
+      enabled: false
     }
   });
 
