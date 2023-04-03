ebf9c1b5e2fcb4b30dcff2d5052d047b353148ec
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-04-03 22:41:14 +1000

    Add ability check logic to user

--- app/models/user.js
+++ app/models/user.js
@@ -3,10 +3,15 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class UserModel extends Model {
   @attr('string') email;
   @attr('string') password;
+  @attr('string') abilities;
 
   @belongsTo('person') person;
 
   get name() {
     return this.person.get('fullName');
+  }
+
+  can(ability) {
+    return this.abilities && this.abilities.split(',').includes(ability);
   }
 }
