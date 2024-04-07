1cd649a83ec4a803dd5cd7b177b7c0969a92bea5
Jan Werkhoven <jan.werkhoven@gmail.com>
2024-04-07 22:29:40 +1000

    Add model for EventAttendee

---
+++ app/models/event-attendee.js
@@ -0,0 +1,12 @@
+import Model, { attr, belongsTo } from '@ember-data/model';
+
+export default class EventAttendeeModel extends Model {
+  @attr('string') firstName;
+  @attr('string') lastName;
+  @attr('string') role;
+  @attr('string') company;
+  @attr('string') email;
+
+  @belongsTo('event') event;
+  @belongsTo('person') person;
+}
